017d9da45708564570c00cadc09accb752b0b465
Valeriajuv <Valeria_juv28@hotmail.com>
2020-10-19 12:06:53 -0500

    Primera tarea

---
+++ Tareas/Division/APP.js
@@ -0,0 +1,13 @@
+function divisionnumeros() {
+    let num1 = 50;
+    let num2 = 30;
+    let division = 0;
+
+    division = num1 / num2;
+
+    document.write(num1 / num2);
+
+    alert("la division de los numeros es:" + division);
+
+    console.log(num1 / num2);
+}
